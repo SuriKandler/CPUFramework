ecf7f5fe18c0560bff50133517a9dbeabf3d75f1
SuriKandler <surikandler@gmail.com>
2024-05-29 13:50:18 +0100

    hw 16

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,11 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string ConnectionString = "Server=.\\SQLExpress01;Database=HeartyHearthDB;Trusted_Connection=true";
+        public static string ConnectionString = "";
+            
         public static DataTable GetDataTable(string sqlstatement) //- take a SQL statement and return a Datatable
         {
+            Debug.Print(sqlstatement);
             DataTable dt = new();
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
@@ -22,7 +25,31 @@ namespace CPUFramework
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+            SetAllColumnsAllowNull(dt);
             return dt;
+        }
+
+        public static void ExecuteSQL(string sqlstatement)
+        {
+            GetDataTable(sqlstatement);
+        }
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach(DataRow r in dt.Rows)
+            {
+                foreach(DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
+                }
+            }
         }
     }
 }
